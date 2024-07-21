690e7efaad775f9bf158eef9a4d129b8b6536a7b
aronsommer <aronsommer@gmail.com>
2024-07-21 18:07:39 +0200

    Build 20240721180426

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "Aron Sommer-Jet Car Jump-1.0";
 const contentToCache = [
-    "Build/0d2e0950fb5b45d5d417b8cfbe8868dd.loader.js",
+    "Build/b1606c27c9e806f9cd78fd7ff758ccea.loader.js",
     "Build/b7f83ab8200ee1cf956413d4336e9d4b.framework.js",
-    "Build/050c290c6cc26f120bbbfcdb4aaf9ceb.data",
-    "Build/688428cd514d55826112bf76483acbcc.wasm",
+    "Build/36209cc572494bb9ad5b32b9998abc83.data",
+    "Build/4c96d1246253103a92d4e92fcc74ad6b.wasm",
     "TemplateData/style.css"
 
 ];
